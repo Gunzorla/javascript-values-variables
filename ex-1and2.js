1efb35303300df646e44486f30c6881fdba5a4c2
Gunzorla <g.gun.n.1991@gmail.com>
2024-10-01 12:30:11 +0700

    Add Excercise 1-4

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,15 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstName = "Karter"
+let lastName = "Anderson"
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
+
+const restaurantName = "Chef's Table by Chef James"
+const billPrice = 15000
+const totalRestaurants = 15000
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+billPrice = 5000
+totalRestaurants = 2500
